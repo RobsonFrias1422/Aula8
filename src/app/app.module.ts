893e301b6eb8dd99fb41f4a9b235dac02461413d
Robson <robsonfrias@gmail.com>
2019-09-25 20:40:52 -0300

    Property Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterpolationBindingComponent } from './interpolation-binding/interpolation-binding.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterpolationBindingComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule,
